459c1fcd4ddeabbdf9232ca1ac10982c13aa2908
Khoa Ngo <khoanl@prosperdigital.com.au>
2024-02-12 19:01:07 +0700

    update: add scss styles

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -39,6 +39,7 @@ export default function PaginatedItems({
       previousLabel="< previous"
       renderOnZeroPageCount={null}
       activeClassName="active"
+      containerClassName='pagination'
     />
   );
 }
